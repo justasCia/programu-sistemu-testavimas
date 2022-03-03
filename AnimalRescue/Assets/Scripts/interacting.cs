c757a36ac546e750d7481d9a21f8d1b1a20deb5d
arvydelis <52567357+arvydelis@users.noreply.github.com>
2022-03-03 13:01:41 +0200

    Laboro gynimas
    gyvunai nebesistasko

--- AnimalRescue/Assets/Scripts/interacting.cs
+++ AnimalRescue/Assets/Scripts/interacting.cs
@@ -10,6 +10,7 @@ public class interacting : MonoBehaviour
     public float range = 1f;
     public LayerMask mask;
     public Transform holdPoint;
+    public float lerpValue = 1f;
 
     public bool equiped = false;
 
@@ -19,7 +20,7 @@ public class interacting : MonoBehaviour
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range, mask))
         {
             //if (animal != null)
-                
+                 
 
             if (Input.GetKeyDown(KeyCode.E))
             {
@@ -43,10 +44,10 @@ public class interacting : MonoBehaviour
             Drop();
         }
 
-        if (animal != null && equiped)
-        {
-            animal.position = holdPoint.position;
-        }
+        //if (animal != null && equiped)
+        //{
+        //    animal.position = Vector3.Lerp(animal.position, holdPoint.position, lerpValue);
+        //}
     }
 
     private void OnDrawGizmos()
@@ -58,7 +59,16 @@ public class interacting : MonoBehaviour
     void PickUp()
     {
         animal = hit.transform;
-        animal.position = holdPoint.position;
+
+        animal.SetParent(holdPoint);
+        animal.localPosition = Vector3.zero;
+        animal.localRotation = Quaternion.Euler(Vector3.zero);
+        animal.transform.Find("Armature").gameObject.SetActive(false);
+
+        animal.GetComponent<Rigidbody>().freezeRotation = true;
+        animal.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        animal.Find("HoldPoint").position = holdPoint.position;
         Debug.Log("Picked up " + hit.transform.name);
         equiped = true;
     }
@@ -71,8 +81,14 @@ public class interacting : MonoBehaviour
 
     void Drop()
     {
+        animal.SetParent(null);
+        animal.transform.Find("Armature").gameObject.SetActive(true);
+        animal.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+
+        animal.GetComponent<FixedJoint>().connectedBody = animal.Find("Armature").Find("Root").GetComponent<Rigidbody>();
+        animal.GetComponent<Rigidbody>().freezeRotation = false;
         Debug.Log("Dropped ");
-        animal.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //animal.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         animal = null;
         equiped = false;
     }
