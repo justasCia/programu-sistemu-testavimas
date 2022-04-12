f73139b7de1339aec4cce43d06cd39179d56adff
Arvydas <arvydas.mazauskas@ktu.edu>
2022-04-12 16:00:03 +0300

    Xray vision uses resources

--- AnimalRescue/Assets/Scripts/XrayToggle.cs
+++ AnimalRescue/Assets/Scripts/XrayToggle.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
 
 public class XrayToggle : MonoBehaviour
 {
     public UniversalRendererData rendererSettings;
 
+    public float maxAmount = 100f;
+    float currentAmount = 50f;
+    public float useAmount = 0.5f;
+    public Slider XrayBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rendererSettings.rendererFeatures[0].SetActive(false);
+
+        currentAmount = maxAmount;
+        XrayBar.maxValue = currentAmount;
+        XrayBar.value = currentAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
+        if (currentAmount > 0)
         {
-            rendererSettings.rendererFeatures[0].SetActive(true);
+            //if (Input.GetKeyDown(KeyCode.X))
+            //XrayOn();
+            if (Input.GetKey(KeyCode.X))
+                XrayOn();
+            else if (Input.GetKeyUp(KeyCode.X))
+                XrayOff();
         }
-        else if (Input.GetKeyUp(KeyCode.X))
-            rendererSettings.rendererFeatures[0].SetActive(false);
+        else
+            XrayOff();
+    }
+
+    void XrayOn()
+    {
+        rendererSettings.rendererFeatures[0].SetActive(true);
+        currentAmount -= useAmount;
+        XrayBar.value = currentAmount;
+    }
+
+    void XrayOff()
+    {
+        rendererSettings.rendererFeatures[0].SetActive(false);
+
     }
 }
