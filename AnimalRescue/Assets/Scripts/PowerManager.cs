998eda93a49a381c0e80f4d522d1ea634b42b8d5
Arvydas <arvydas.mazauskas@ktu.edu>
2022-05-24 18:55:16 +0300

    powerupps UI

--- AnimalRescue/Assets/Scripts/PowerManager.cs
+++ AnimalRescue/Assets/Scripts/PowerManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerManager : MonoBehaviour
 {
     //Transform animal = null;
+    public float ratScale = .25f;
+
+    public Image powerUpHolder;
+    public Sprite empty;
+    public Sprite speedUp;
+    public Sprite sizeDown;
+    public Sprite wallClimb;
 
     private void Start() 
     {
@@ -26,13 +34,16 @@ public class PowerManager : MonoBehaviour
         {
             case "Dog":
                 gameObject.GetComponent<PlayerMovement>().speed = 10f;
+                ChangeImage(speedUp);
                 break;
             case "Cat":
                 gameObject.GetComponent<WallClimbing>().enabled = true;
+                ChangeImage(wallClimb);
                 break;
             case "Rat":
                 //transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.25f, 0.25f, 0.25f), 0.1f);
-                transform.localScale =  new Vector3(0.25f, 0.25f, 0.25f);
+                transform.localScale =  new Vector3(ratScale, ratScale, ratScale);
+                ChangeImage(sizeDown);
                 break;
         }
     }
@@ -43,5 +54,11 @@ public class PowerManager : MonoBehaviour
         gameObject.GetComponent<WallClimbing>().enabled = false;
         //transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1f, 1f, 1f), 0.1f);
         transform.localScale = new Vector3(1f, 1f, 1f);
+        ChangeImage(empty);
+    }
+
+    void ChangeImage(Sprite img)
+    {
+        powerUpHolder.sprite = img;
     }
 }
